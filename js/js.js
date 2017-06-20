5ac27ea204f1b832c2d7bd158d23984337098ad1
Sam <Sam.hak1995@gmail.com>
2017-06-21 00:48:12 +0400

    enter

---
+++ js/js.js
@@ -0,0 +1,26 @@
+"use strict";
+// number Fibonacci
+var k;
+k = +prompt("enter number", "");
+var a1 = 1;
+var a2 = 1;
+var fib = 1;
+for(var i = 2; i < k; i++){
+    fib = a1 + a2;
+    a1 = a2;
+    a2 = fib;
+}
+document.write("<br>" + fib);
+
+// 2th method 
+var n;
+n = +prompt("enter number", "");
+function fibonacci(n){
+    if(n == 1 || n == 0){
+        return n;
+    }else{
+        return n = fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+document.write(fibonacci(n));
+
